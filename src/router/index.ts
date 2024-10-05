d5f20989caa9929508933261a3b3aabab3a0c790
ZeroAurora <zeroaurora233@outlook.com>
2024-10-05 16:58:00 +0800

    add spatial plot

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
       path: "/daily-temperature",
       name: "daily-temperature",
       component: () => import("../views/DailyTemperatureView.vue"),
+    },
+    {
+      path: "/spatial-plot",
+      name: "spatial-plot",
+      component: () => import("../views/SpatialPlotView.vue"),
     },
   ],
 });
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <h1>hii</h1>
+  <ol>
+    <li>
+      <RouterLink to="/daily-temperature">Daily Temperature</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/spatial-plot">Spatial Plot</RouterLink>
+    </li>
+  </ol>
+</template>
