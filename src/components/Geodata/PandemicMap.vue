684b10f7080979f42a1e8bc7eeeb1e3d884bca5f
ZeroAurora <zeroaurora233@outlook.com>
2024-11-07 22:31:49 +0800

    rework on geodata ok

--- src/components/Geodata/PandemicMap.vue
+++ src/components/Geodata/PandemicMap.vue
@@ -1,37 +1,14 @@
 <script setup lang="ts">
 import * as echarts from "echarts";
-import { ref, watch } from "vue";
+import { onMounted, ref } from "vue";
 import VChart from "vue-echarts";
-
-const props = defineProps<
-  {
-    type: "confirmed" | "suspected" | "cured" | "dead" | "cumulated";
-    date: string;
-  }
->();
 
 const option = ref({});
 
-const typeToColIndex = {
-  confirmed: 2,
-  suspected: 3,
-  cured: 4,
-  dead: 5,
-  cumulated: 6,
-};
-
-const typeToTitle = {
-  confirmed: "确诊",
-  suspected: "疑似",
-  cured: "治愈",
-  dead: "死亡",
-  cumulated: "累计",
-};
-
-let data: any[][];
 let geojson: any;
+let data: Record<string, any[][]>;
 
-watch(props, async (newProps) => {
+onMounted(async () => {
   if (!geojson) {
     geojson = await (await fetch("/data/3/geojson.json")).json();
     echarts.registerMap("china", geojson);
@@ -41,55 +18,59 @@ watch(props, async (newProps) => {
     data = await (await fetch("/data/3/data.json")).json();
   }
 
-  const type = newProps.type;
-  const date = newProps.date;
+  const options: Record<string, any>[] = [];
 
-  const colIndex = typeToColIndex[type];
-  const title = typeToTitle[type];
-
-  const seriesData = data
-    .filter(d => d[0] === date)
-    .map(d => ({
-      name: d[1],
-      value: d[colIndex],
+  for (const updateTime of Object.keys(data)) {
+    const info = data[updateTime];
+    const mappedInfo = info.map(item => ({
+      name: item[0],
+      value: item[1],
     }));
-
-  // dynamically compute the max value
-  // the max value is too large so multiply it by 0.02
-  const visualMapMax = data.reduce((max, d) => Math.max(max, d[colIndex]), 0) * 0.02;
+    options.push({
+      title: {
+        text: `${updateTime} 确诊`,
+      },
+      series: {
+        data: mappedInfo,
+      },
+    });
+  }
 
   option.value = {
     visualMap: {
       left: "right",
       min: 0,
-      max: visualMapMax,
+      max: 100000,
       inRange: {
         color: ["lightskyblue", "yellow", "orangered"],
       },
       text: ["High", "Low"],
       calculable: true,
     },
-    title: {
-      text: `${title} - ${date}`,
-    },
     tooltip: {
       trigger: "item",
       showDelay: 0,
       transitionDuration: 0.2,
     },
-    series: [
-      {
-        name: title,
-        type: "map",
-        roam: true,
-        map: "china",
-        data: seriesData,
-      },
-    ],
+    series: {
+      name: "确诊",
+      type: "map",
+      roam: true,
+      map: "china",
+    },
+    timeline: {
+      axisType: "category",
+      data: Object.keys(data),
+      playInterval: 500,
+      checkpointStyle: {
+        animationDuration: 100,
+      }
+    },
+    options,
   };
-}, { immediate: true });
+});
 </script>
 
 <template>
-  <VChart :option :update-options="{ notMerge: true }" :autoresize="true" />
+  <VChart :option :autoresize="true" />
 </template>
